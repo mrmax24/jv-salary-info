94591b3a6f56c0a371a2e8886959497151a36ece
Maks Hrytsyna <101352509+mrmax24@users.noreply.github.com>
2022-08-24 13:57:15 +0300

    Update SalaryInfo.java

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -6,6 +6,7 @@ import java.time.format.DateTimeFormatter;
 public class SalaryInfo {
     private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("dd.MM.yyyy");
     private static final int DATE_POSITION = 0;
+    private static final int NAME_POSITION = 1;
     private static final int HOURS_POSITION = 2;
     private static final int SALARY_POSITION = 3;
 
@@ -14,20 +15,18 @@ public class SalaryInfo {
         LocalDate localDateTo = LocalDate.parse(dateTo, FORMATTER);
         StringBuilder result = new StringBuilder().append("Report for period ")
                 .append(dateFrom).append(" - ").append(dateTo).append(System.lineSeparator());
-        String[] arrayOfData;
+        String[] rowSplitedData;
         for (String name : names) {
             int salary = 0;
             for (String line : data) {
-                arrayOfData = line.split(" ");
-                if (!line.contains(name)) {
-                    continue;
-                }
-                LocalDate currentDate = LocalDate.parse(arrayOfData[DATE_POSITION], FORMATTER);
-                if (localDateFrom.compareTo(currentDate) <= 0
-                        && localDateTo.compareTo(currentDate) >= 0
-                        && arrayOfData[1].equals(name)) {
-                    salary += (Integer.parseInt(arrayOfData[HOURS_POSITION])
-                            * Integer.parseInt(arrayOfData[SALARY_POSITION]));
+                rowSplitedData = line.split(" ");
+                LocalDate currentDate = LocalDate.parse(rowSplitedData[DATE_POSITION], FORMATTER);
+                if (name.equals(rowSplitedData[NAME_POSITION])) {
+                    if (currentDate.isEqual(localDateFrom) || currentDate.isAfter(localDateFrom)
+                            && (currentDate.isEqual(localDateTo) || currentDate.isBefore(localDateTo))) {
+                        salary += (Integer.parseInt(rowSplitedData[HOURS_POSITION])
+                                * Integer.parseInt(rowSplitedData[SALARY_POSITION]));
+                    }
                 }
             }
             result.append(name).append(" - ").append(salary).append(System.lineSeparator());
